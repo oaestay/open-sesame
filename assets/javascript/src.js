f87df527e24e684cb1373d5e33b8b75bf440ff65
oaestay <oaestay@uc.cl>
2018-02-08 09:37:14 -0300

    fix(): Add HTTPS

--- assets/javascript/src.js
+++ assets/javascript/src.js
@@ -1,6 +1,6 @@
 function openDoor() {
   var request = new XMLHttpRequest();
-  request.open('POST', 'http://192.168.86.77:8123/api/services/switch/turn_on', true);
+  request.open('POST', 'https://192.168.86.77:8123/api/services/switch/turn_on', true);
   request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
   request.send({'entity_id': 'switch.main_door'});
 }
